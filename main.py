59cfe4156a4ca5ad34611f171614afd462d04a5d
Thomas van den Hurk <thomas_vandenhurk@hotmail.com>
2020-09-30 22:19:07 +0200

    [IMP] - #1 - read prep and merge files

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+from src.read_data import read_portefeuille
+
+
+def main():
+    read_portefeuille()
+
+
 if __name__ == '__main__':
-    print('Do something')
+    main()
---
+++ src/read_data.py
@@ -0,0 +1,36 @@
+import os
+from functools import reduce
+from os import listdir
+
+import pandas as pd
+
+
+def read_portefeuille(path_to_data_dir='data', suffix='.csv'):
+    # obtain all filesnames
+    filenames = list_filenames(path_to_data_dir, suffix)
+
+    # read and prep files
+    portefeuille = []
+    for file in filenames:
+        date = file.replace('.csv', '')
+        data_month = pd.read_csv(os.path.join(path_to_data_dir, file))
+        data_month.drop(columns=['Slotkoers', 'Lokale waarde'], axis=1, inplace=True)
+        data_month['Waarde in EUR'] = pd.to_numeric(data_month['Waarde in EUR'].str.replace(',', '.'))
+        data_month = data_month.rename(columns={'Aantal': date + ' (aantal)', 'Waarde in EUR': date + ' (waarde)'})
+        portefeuille.append(data_month)
+
+    # merge all files
+    portefeuille = reduce(lambda x, y: pd.merge(x, y, on=['Product', 'Symbool/ISIN'], how='outer'), portefeuille)
+
+
+def list_filenames(path_to_data_dir, suffix):
+    """
+    List all file names in path_to_data_dir with a certain suffix.
+
+    :param path_to_data_dir: path to data folder.
+    :param suffix: suffix in which file names must end.
+    :return: all file names existing in path_to_data_dir with suffix
+    """
+
+    filenames = listdir(path_to_data_dir)
+    return [filename for filename in filenames if filename.endswith(suffix)]
